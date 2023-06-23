deca1fa691ef371f86ca9fc691fe1ec47d57d143
Rohit Chalak <rohit.s.chalak@gmail.com>
2023-06-24 01:26:38 +0530

    competitions update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,97 @@
 import React from "react";
 import maincss from "./css/main.module.css";
+const img =
+  "https://images.unsplash.com/photo-1625637870692-ddd9cd870757?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1lbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60";
 const Main = () => {
   return (
     <>
       <div className={maincss.main}>
         <h1>Featured Components</h1>
         <div className={maincss.boxes}>
-          <div className={`${maincss.item} ${maincss.item1}`}>1</div>
-          <div className={`${maincss.item} ${maincss.itemspan2}`}>2</div>
-          <div className={`${maincss.item} ${maincss.item3}`}>3</div>
-          <div className={`${maincss.item} ${maincss.item4}`}>4</div>
-          <div className={`${maincss.item} ${maincss.item5}`}>5</div>
+          <div
+            className={`${maincss.item} ${maincss.item1}`}
+            style={{
+              backgroundImage: `url(${img})`,
+            }}
+          >
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div
+            className={`${maincss.item} ${maincss.itemspan2}`}
+            style={{
+              backgroundImage: `url(${img})`,
+            }}
+          >
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div
+            className={`${maincss.item} ${maincss.item3}`}
+            style={{
+              backgroundImage: `url(${img})`,
+            }}
+          >
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div
+            className={`${maincss.item} ${maincss.item4}`}
+            style={{
+              backgroundImage: `url(${img})`,
+            }}
+          >
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div
+            className={`${maincss.item} ${maincss.item5}`}
+            style={{
+              backgroundImage: `url(${img})`,
+            }}
+          >
+            <div className={maincss.box_text}>asasd</div>
+          </div>
         </div>
         <h1>Modules</h1>
         <div className={maincss.boxes2}>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
-          <div className={`${maincss.box} ${maincss.item5}`}></div>
+          <div
+            className={`${maincss.box} ${maincss.item5}`}
+            style={{
+              backgroundImage: `url(${img})`,
+            }}
+          >
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
+          <div className={`${maincss.box} ${maincss.item5}`}>
+            <div className={maincss.box_text}>asasd</div>
+          </div>
         </div>
       </div>
     </>
--- src/components/TeamBoxes.jsx
+++ src/components/TeamBoxes.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import teambox from "../components/css/teamboxcss.module.css";
 import TeamsOneBox from "./teamsOneBox";
+import teams_r1 from "./images/teams_rectangle1.svg";
+import teams_r2 from "./images/teams_rectangle2.svg";
+import teams_r3 from "./images/teams_rectangle3.svg";
+import teams_r4 from "./images/teams_rectangle4.svg";
+import teams_r5 from "./images/teams_rectangle5.svg";
+import teams_ptt from "./images/teams_pattern.svg";
 
 function getBoxes(num) {
   const array = [];
@@ -24,25 +30,51 @@ const TeamBoxes = () => {
     <>
       <main>
         <h1>Public Relations & Branding</h1>
-        <div className={teambox.pr}>{getBoxes(3)}</div>
+        <div className={teambox.pr}>
+          <img src={teams_r1} className={teambox.tr1} alt="" />
+          <img src={teams_ptt} className={teambox.teams_ptt1} alt="" />
+          {getBoxes(3)}
+        </div>
         <h1>Corporate Relations & Marketing</h1>
-        <div className={teambox.pr}>{getBoxes(4)}</div>
+        <div className={teambox.pr}>
+          <img src={teams_r2} className={teambox.tr2} alt="" />
+          <img src={teams_ptt} className={teambox.teams_ptt2} alt="" />
+          {getBoxes(4)}
+        </div>
         <h1>Events</h1>
-        <div className={teambox.pr}>{getBoxes(3)}</div>
+        <div className={teambox.pr}>
+          <img src={teams_r3} className={teambox.tr3} alt="" />
+          <img src={teams_ptt} className={teambox.teams_ptt3} alt="" />
+          {getBoxes(3)}
+        </div>
         <h1>Creatives</h1>
-        <div className={teambox.pr}>{getBoxes(3)}</div>
+        <div className={teambox.pr}>
+          <img src={teams_r4} className={teambox.tr4} alt="" />
+          <img src={teams_ptt} className={teambox.teams_ptt4} alt="" />
+          {getBoxes(3)}
+        </div>
         <h1>IITG MUN</h1>
-        <div className={teambox.pr}>{getBoxes(2)}</div>
+        <div className={teambox.pr}>
+          <img src={teams_ptt} className={teambox.teams_ptt5} alt="" />
+          {getBoxes(2)}
+        </div>
         <h1 className={teambox.pr}>
           <div className={teambox.for2heading}>Web Ops</div>
           <div className={teambox.for2heading}>App Ops</div>
         </h1>
-        <div className={teambox.pr}>{getBoxes(2)}</div>
+        <div className={teambox.pr}>
+          <img src={teams_r5} className={teambox.tr5} alt="" />
+          {getBoxes(2)}
+        </div>
         <h1 className={teambox.pr}>
           <div className={teambox.for2heading}>Convener</div>
           <div className={teambox.for2heading}>Finance head</div>
         </h1>
-        <div className={teambox.pr}>{getBoxes(2)}</div>
+        <div className={teambox.pr}>
+          <img src={teams_ptt} className={teambox.teams_ptt6} alt="" />
+          <img src={teams_ptt} className={teambox.teams_ptt7} alt="" />
+          {getBoxes(2)}
+        </div>
       </main>
     </>
   );
